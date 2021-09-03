86cbe58fcafa43427167c623612ab110538ec437
dagriel <dagriel@gmail.com>
2021-09-02 21:37:57 -0300

    add filterBy, handling data binding events

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -8,69 +8,27 @@ import { Course } from './course';
 })
 export class CourseListComponent implements OnInit {
 
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+
+  _courses: Course[] = [];
+
+  _filterBy: string;
 
   constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
-    this.courses = this.courseService.retrieveAll();
+    this._courses = this.courseService.retrieveAll();
 
-    /* this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: 'assets/images/forms.png',
-        price: 99.99,
-        code: 'XPS-8796',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: 'August, 31, 2021',
-        description: ''
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTP',
-        imageUrl: 'assets/images/http.png',
-        price: 45.99,
-        code: 'LKL-1094',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'September, 1, 2021',
-        description: ''
-      },
-      {
-        id: 3,
-        name: 'Angular: Animations',
-        imageUrl: 'assets/images/animations.png',
-        price: 34.99,
-        code: 'LCZ-1195',
-        duration: 60,
-        rating: 3.6,
-        releaseDate: 'September, 2, 2021',
-        description: ''
-      },
-      {
-        id: 4,
-        name: 'Angular: CLI',
-        imageUrl: 'assets/images/cli.png',
-        price: 29.99,
-        code: 'DGR-0874',
-        duration: 30,
-        rating: 5,
-        releaseDate: 'September, 2, 2021',
-        description: ''
-      },
-      {
-        id: 5,
-        name: 'Angular: Router',
-        imageUrl: 'assets/images/router.png',
-        price: 19.99,
-        code: 'LPS-0091',
-        duration: 20,
-        rating: 4.1,
-        releaseDate: 'September, 2, 2021',
-        description: ''
-      }
-    ] */
+    this.filteredCourses = this._courses;
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+
+  get filter() {
+      return this._filterBy;
   }
 }
