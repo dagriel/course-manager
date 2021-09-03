3695bf7ce74742b634920f3a005c1be2d1c937fe
dagriel <dagriel@gmail.com>
2021-09-03 16:29:11 -0300

    work with forms and templates

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -14,6 +14,13 @@ export class CourseService {
     return COURSES.find((courseInterator: Course) => courseInterator.id === id);
   }
 
+  save(course: Course): void {
+    if(course.id) {
+        const index = COURSES.findIndex((courseInterator: Course) => courseInterator.id === course.id);
+        COURSES[index] = course;
+    }
+}
+
 }
 
 var COURSES: Course[] = [
