644b136c0b6d1d606cb143254d0f17195246eafc
dagriel <dagriel@gmail.com>
2021-09-03 14:26:08 -0300

    add course information form

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -1,8 +1,5 @@
 import { Course } from './course';
 import { Injectable } from '@angular/core';
-//import { HttpClient } from '@angular/common/http';
-//import { Observable } from 'rxjs';
-
 
 @Injectable({
     providedIn: 'root'
@@ -11,31 +8,11 @@ export class CourseService {
 
   retrieveAll(): Course[] {
     return COURSES;
-}
+  }
 
-    /* private coursesUrl: string = 'http://localhost:3100/api/courses';
-
-    constructor(private httpClient: HttpClient) { }
-
-    retrieveAll(): Observable<Course[]> {
-        return this.httpClient.get<Course[]>(this.coursesUrl);
-    }
-
-    retrieveById(id: number): Observable<Course> {
-        return this.httpClient.get<Course>(`${this.coursesUrl}/${id}`);
-    }
-
-    save(course: Course): Observable<Course> {
-        if(course.id) {
-            return this.httpClient.put<Course>(`${this.coursesUrl}/${course.id}`, course);
-        } else {
-            return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
-        }
-    }
-
-    deleteById(id: number): Observable<any> {
-        return this.httpClient.delete<any>(`${this.coursesUrl}/${id}`);
-    } */
+  retrieveById(id: number): Course {
+    return COURSES.find((courseInterator: Course) => courseInterator.id === id);
+  }
 
 }
 
