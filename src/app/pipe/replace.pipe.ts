7efe96db5f0a1a7ec4bcfa5e9a7c791abf5d88e9
dagriel <dagriel@gmail.com>
2021-09-03 13:33:41 -0300

    enable routes to access components

--- src/app/pipe/replace.pipe.ts
+++ src/app/pipe/replace.pipe.ts
@@ -4,9 +4,11 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "replace"
 })
 export class ReplacePipe implements PipeTransform {
-transform(value: string, char: string, valueToReplace: string){
-  return value.replace(char, valueToReplace);
 
-}
+  transform(value: string, char: string, valueToReplace: string){
+
+    return value.replace(char, valueToReplace);
+
+  }
 
 }
